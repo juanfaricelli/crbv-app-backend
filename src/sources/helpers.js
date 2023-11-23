5b37eefc1f887872429baf91adab7b19d97b8399
Juan Faricelli <juan.faricelli@outlook.com>
2023-11-23 23:11:22 +0100

    changing datepicker input type

--- src/sources/helpers.js
+++ src/sources/helpers.js
@@ -101,7 +101,7 @@ const formField = {
     placeholder = 'placeholder',
     name = 'inputname',
   }) => ({
-    inputType: 'number',
+    inputType: 'date',
     label,
     placeholder,
     name,
